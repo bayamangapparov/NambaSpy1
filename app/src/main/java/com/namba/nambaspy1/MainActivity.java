cf6ff3a88784024d70bcc6186751ba09a278a3b6
Bayaman Gapparov <bayamangapparov@gmail.com>
2018-05-23 14:34:03 +0600

    subCategory recycler done

--- app/src/main/java/com/namba/nambaspy1/MainActivity.java
+++ app/src/main/java/com/namba/nambaspy1/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
         startActivity(intent);
 
-        //circle image
+    /*    //circle image
         Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.depp);
         Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
 
         ImageView circularImageView = (ImageView) findViewById(R.id.imageAvatar);
         circularImageView.setImageBitmap(circularBitmap);
-
+*/
     }
     
     public void onClick(View view) {
--- app/src/main/java/com/namba/nambaspy1/SubCategoryActivity.java
+++ app/src/main/java/com/namba/nambaspy1/SubCategoryActivity.java
@@ -2,18 +2,20 @@ package com.namba.nambaspy1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubCategoryActivity extends AppCompatActivity {
 
     private RecyclerView subCategoryRecyclerView;
-    private CategoryAdapter subCategoryAdapter;
-    List<Category> subCategoryList;
+    private NearPlaceAdapter subCategoryAdapter;
+    List<NearPlace> subCategoryList;
 
     private final String TAG = SubCategoryActivity.class.getSimpleName();
 
@@ -21,6 +23,11 @@ public class SubCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_category);
+
+
+        //Calling methods
+        initSubCategoryRecyclerView();
+        initSubCategoryList();
 
 
         Toolbar toolbar = findViewById(R.id.mToolbar);
@@ -41,4 +48,26 @@ public class SubCategoryActivity extends AppCompatActivity {
         super.onDestroy();
 
     }
+
+    void initSubCategoryRecyclerView(){
+        subCategoryRecyclerView = findViewById(R.id.subCategory_recycler);
+        subCategoryRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        subCategoryAdapter = new NearPlaceAdapter(this);
+        subCategoryRecyclerView.setAdapter(subCategoryAdapter);
+
+    }
+
+    void initSubCategoryList(){
+        subCategoryList = new ArrayList<>();
+        subCategoryList.add(new NearPlace("Ормон хан", R.drawable.chay_ic));
+        subCategoryList.add(new NearPlace("Гурман", R.drawable.fastfood_ic));
+        subCategoryList.add(new NearPlace("Фаиза", R.drawable.chay_ic));
+        subCategoryList.add(new NearPlace("Таксым", R.drawable.chay_ic));
+        subCategoryList.add(new NearPlace("Обед.кг", R.drawable.restaurant));
+        subCategoryList.add(new NearPlace("Ормон хан", R.drawable.chay_ic));
+
+        subCategoryAdapter.setNearPlaceList(subCategoryList);
+
+    }
+
 }
