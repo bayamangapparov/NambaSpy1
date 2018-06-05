dfd7277c44e6db7e53480cca29bf771afed828f2
Talgat <talgat.asanbaev@gmail.com>
2018-06-05 12:20:19 +0600

    "GalleryActivity: добавлен ViewPager для слайда изображений - 2"

--- app/src/main/java/com/namba/nambaspy1/GalleryActivity.java
+++ app/src/main/java/com/namba/nambaspy1/GalleryActivity.java
@@ -43,8 +43,6 @@ public class GalleryActivity extends AppCompatActivity {
     private Animator mCurrentAnimator;
 
     private int mShortAnimationDuration;
-    Bitmap bm;
-    ImageView expandedImageView;
     LinearLayout linearLayout;
     Rect startBounds;
     float startScaleFinal;
@@ -72,6 +70,9 @@ public class GalleryActivity extends AppCompatActivity {
         initImageRecyclerView();
         initImages();
 
+
+        linearLayout = findViewById(R.id.layou);
+
         viewPager = findViewById(R.id.imageViewPager);
         pagerAdapter = new GalleryPagerAdapter(this, imagesList);
         viewPager.setAdapter(pagerAdapter);
@@ -98,35 +99,16 @@ public class GalleryActivity extends AppCompatActivity {
         imagesList = images;
     }
 
-    private void zoomImageFromThumb(final View thumbView, String link) {
-        // If there's an animation in progress, cancel it
-        // immediately and proceed with this one.
+    private void zoomImageFromThumb(final View thumbView) {
         if (mCurrentAnimator != null) {
             mCurrentAnimator.cancel();
         }
 
-        linearLayout = findViewById(R.id.layou);
         linearLayout.setVisibility(View.VISIBLE);
-        // Load the high-resolution "zoomed-in" image.
-        /*expandedImageView = findViewById(
-                R.id.expanded_image);*/
-        //expandedImageView.setImageResource(imageResId);
-        Picasso.get()
-                .load(link)
-                .placeholder(R.drawable.place_image)
-                .into(expandedImageView);
-
-        // Calculate the starting and ending bounds for the zoomed-in image.
-        // This step involves lots of math. Yay, math.
         startBounds = new Rect();
         final Rect finalBounds = new Rect();
         final Point globalOffset = new Point();
 
-        // The start bounds are the global visible rectangle of the thumbnail,
-        // and the final bounds are the global visible rectangle of the container
-        // view. Also set the container view's offset as the origin for the
-        // bounds, since that's the origin for the positioning animation
-        // properties (X, Y).
         thumbView.getGlobalVisibleRect(startBounds);
 
         findViewById(R.id.container)
@@ -135,14 +117,9 @@ public class GalleryActivity extends AppCompatActivity {
         startBounds.offset(-globalOffset.x, -globalOffset.y);
         finalBounds.offset(-globalOffset.x, -globalOffset.y);
 
-        // Adjust the start bounds to be the same aspect ratio as the final
-        // bounds using the "center crop" technique. This prevents undesirable
-        // stretching during the animation. Also calculate the start scaling
-        // factor (the end scaling factor is always 1.0).
         float startScale;
         if ((float) finalBounds.width() / finalBounds.height()
                 > (float) startBounds.width() / startBounds.height()) {
-            // Extend start bounds horizontally
             startScale = (float) startBounds.height() / finalBounds.height();
             float startWidth = startScale * finalBounds.width();
             float deltaWidth = (startWidth - startBounds.width()) / 2;
@@ -157,29 +134,22 @@ public class GalleryActivity extends AppCompatActivity {
             startBounds.bottom += deltaHeight;
         }
 
-        // Hide the thumbnail and show the zoomed-in view. When the animation
-        // begins, it will position the zoomed-in view in the place of the
-        // thumbnail.
-        thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
 
-        // Set the pivot point for SCALE_X and SCALE_Y transformations
-        // to the top-left corner of the zoomed-in view (the default
-        // is the center of the view).
-        expandedImageView.setPivotX(0f);
-        expandedImageView.setPivotY(0f);
 
-        // Construct and run the parallel animation of the four translation and
-        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+
+        viewPager.setPivotX(0f);
+        viewPager.setPivotY(0f);
+
+
         AnimatorSet set = new AnimatorSet();
         set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                .play(ObjectAnimator.ofFloat(viewPager, View.X,
                         startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                .with(ObjectAnimator.ofFloat(viewPager, View.Y,
                         startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                .with(ObjectAnimator.ofFloat(viewPager, View.SCALE_X,
                         startScale, 1f))
-                .with(ObjectAnimator.ofFloat(expandedImageView,
+                .with(ObjectAnimator.ofFloat(viewPager,
                         View.SCALE_Y, startScale, 1f));
         set.setDuration(mShortAnimationDuration);
         set.setInterpolator(new DecelerateInterpolator());
@@ -197,55 +167,9 @@ public class GalleryActivity extends AppCompatActivity {
         set.start();
         mCurrentAnimator = set;
 
-        // Upon clicking the zoomed-in image, it should zoom back down
-        // to the original bounds and show the thumbnail instead of
-        // the expanded image.
-        //final float startScaleFinal = startScale;
+
         startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
-                }
 
-                // Animate the four positioning/sizing properties in parallel,
-                // back to their original values.
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.Y,startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        linearLayout.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        linearLayout.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
-            }
-        });
     }
 
 
@@ -254,26 +178,59 @@ public class GalleryActivity extends AppCompatActivity {
             mCurrentAnimator.cancel();
         }
 
+        currentView = recyclerView.getChildAt(viewPager.getCurrentItem());
+
+        currentView.getGlobalVisibleRect(startBounds);
+
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+
+        currentView.setAlpha(0f);
+
 
         AnimatorSet set = new AnimatorSet();
         set.play(ObjectAnimator
-                .ofFloat(expandedImageView, View.X, startBounds.left))
+                .ofFloat(viewPager, View.X, startBounds.left))
                 .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
+                        .ofFloat(viewPager,
                                 View.Y, startBounds.top))
                 .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
+                        .ofFloat(viewPager,
                                 View.SCALE_X, startScaleFinal))
                 .with(ObjectAnimator
-                        .ofFloat(expandedImageView,
+                        .ofFloat(viewPager,
                                 View.SCALE_Y, startScaleFinal));
         set.setDuration(mShortAnimationDuration);
         set.setInterpolator(new DecelerateInterpolator());
         set.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
+
                 currentView.setAlpha(1f);
-                expandedImageView.setVisibility(View.GONE);
                 linearLayout.setVisibility(View.GONE);
                 mCurrentAnimator = null;
             }
@@ -281,7 +238,6 @@ public class GalleryActivity extends AppCompatActivity {
             @Override
             public void onAnimationCancel(Animator animation) {
                 currentView.setAlpha(1f);
-                expandedImageView.setVisibility(View.GONE);
                 linearLayout.setVisibility(View.GONE);
                 mCurrentAnimator = null;
             }
@@ -300,7 +256,7 @@ public class GalleryActivity extends AppCompatActivity {
         }*/
 
         if (linearLayout.getVisibility() == View.VISIBLE) {
-            linearLayout.setVisibility(View.GONE);
+            hideExtendedImageWithAnimation();
         } else {
             super.onBackPressed();
         }
@@ -361,10 +317,8 @@ public class GalleryActivity extends AppCompatActivity {
                 imageView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //zoomImageFromThumb(v, links.get(getAdapterPosition()));
                         viewPager.setCurrentItem(getAdapterPosition(), false);
-                        linearLayout = findViewById(R.id.layou);
-                        linearLayout.setVisibility(View.VISIBLE);
+                        zoomImageFromThumb(v);
                         currentView = v;
                     }
                 });
